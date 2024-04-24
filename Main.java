3f2b44da22a8fa20fe546971fa35e388a9c06c5d
Dan-Corzo <danielcorzo101@gmail.com>
2024-04-24 14:14:33 -0400

    Agregar Sentencias Condicionales

--- Main.java
+++ Main.java
@@ -5,7 +5,8 @@ public class Main {
 
         try (Scanner sc = new Scanner(System.in)) {
             System.out.println("Define los coeficientes de la ecuación cuadrática: (ax^2 + bx + c = 0)");
-            System.out.print("a: ");        
+
+            System.out.print("a: ");
             double a = sc.nextDouble();
 
             System.out.print("b: ");
@@ -17,6 +18,13 @@ public class Main {
             Ecuacion ecuacion = new Ecuacion(a, b, c);
 
             double[] soluciones = ecuacion.resolver();
+            if (soluciones.length == 0) {
+                System.out.println("La ecuación no tiene soluciones reales.");
+            } else if (soluciones.length == 1) {
+                System.out.println("La ecuación tiene una solución real: " + soluciones[0]);
+            } else {
+                System.out.println("La ecuación tiene dos soluciones reales: " + soluciones[0] + " y " + soluciones[1]);
+            }
         }
     }
 }
